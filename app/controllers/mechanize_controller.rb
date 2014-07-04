f2f16b95d220ee19d80808bea212b49e78c4f1d3
Honey Comb <honeycomb8080@gmail.com>
2014-07-03 22:08:21 -0700

    making updates to the mechanize

--- app/controllers/mechanize_controller.rb
+++ app/controllers/mechanize_controller.rb
@@ -5,109 +5,59 @@ require "rubygems"
 require "open-uri"
 
 
-# vvv 1. ARTICLE GETTER vvv
-# vvv [to make ready, remove "take(1)"] vvv
+def get_article_data(pg = 2, article_date = Date.today)
+  agent = Mechanize.new
+  tc = agent.get('http://www.techcrunch.com')
+  article_pg_count = 0
 
-# agent = Mechanize.new
-# tc = agent.get('http://www.techcrunch.com')
-# article_pg_count = 0
-# pg = 2
-
-# tc.root.css('h2.post-title').take(1).each do |this|
-#   @link  =  this.css('a')[0]['href']
-#   title  =  this.css('a')[0].text
-#   m_link =  this.css('a')[0]
-#   url = Mechanize::Page::Link.new(m_link, agent, tc)
-#   url = url.click
-#   article_pg_count += 1
-#   puts "Article: #{article_pg_count}"
-
-
-# vvv 2. NEXT PAGE GETTER (relies on 1: link, article_pg_count) vvv
-# vvv [to prep, insert page scraping, parsing functions]
-
-#   if article_pg_count == 20
-#     tc = tc.link_with(:href => %r{page/#{pg}}i).click
-#     tc
-#     pg += 1
-#     article_pg_count = 0
-#   end
-# end
-
-
-# vvv 3. TEXT STRIPPER vvv (relies on 2: @link)
-# vvv [to get ready, insert parsing function where "puts @article_text" is] vvv
-
-# s_agent = Mechanize.new
-# tc_article = s_agent.get(@link)
-# html_elem = tc_article.at('div.article-entry')
-# case html_elem
-# when html_elem.css('p').each do |t|
-#     @article_text = t.text.strip
-#     puts @article_text
-#   end
-# end
+  until article_date === Date.today - 2
+    # gets article date
+    tc.root.css('li.river-block').each do |link|
+      article_date = link.css('time')[0]['datetime']
+      article_date = Date.parse(article_date)
+    end
+    tc.root.css('h2.post-title').each do |this|
+      # gets the link
+      @link  =  this.css('a')[0]['href']
+      puts @link
+      # gets the title
+      title  =  this.css('a')[0].text
+      puts title
+      # creates the mechanize object to the article for scraping later
+      m_link =  this.css('a')[0]
+      url = Mechanize::Page::Link.new(m_link, agent, tc)
+      # url = url.click <-- I can use this to change course, scrape articles while crawling
+      article_pg_count += 1
+      # clicks the next page of links
+      if article_pg_count == 20
+        tc = tc.link_with(:href => %r{page/#{pg}}i).click
+        tc
+        pg += 1
+        puts pg
+        article_pg_count = 0
+      end
+    end
+  end
+end
 
 
+# text stripper, put in a URL for @link to make it happen
+def strip_text(@link)
+  s_agent = Mechanize.new
+  tc_article = s_agent.get(@link)
+  html_elem = tc_article.at('div.article-entry')
+  case html_elem
+  when html_elem.css('p').each do |t|
+      @article_text = t.text.strip
+      puts @article_text
+    end
+  end
+end
 
-# vvv 4. GETS ARTICLE DATE (relies on 1: link) vvv
-# vvv [to prep, replace "pp article_date" with ActiveRecord code] vvv
-  
-  # tc.root.css('li.river-block').each do |link|
-  #   article_date = link.css('time')[0]['datetime']
-  #   article_date = Date.parse(article_date)
-  #   pp article_date
-  # end
-
-
-
-
-# vvv 5. LIMITS ARTICLE GETS BY DATE vvv
-# vvv [to prep, insert at top of function]
-
-# def get_articles(pg = 2, article_date = Date.today)
-#   ...
-#   until article_date === Date.today - 2
-#     tc.root.css('li.river-block')....
-#       
-
-
-
-# vvv TEST DATA vvv
-
-# array = ['alpha', 'Bravo', 'charlie', 'Delta', 'Echo', 'foxtrot', 'golf', '500',
-#   'Hotel', 'Igloo', 'Juliet', 'Kilo', '10,000', 'a', 'target', 'window', 'of',
-#   '“sometime', 'in', 'July”.', 'Word', 'on', 'the', 'Oculus', 'fan', 'forums',
-#   '(Developer', 'Kit', '#2', '45,000+', 'pre-orders', 'while:', 'while', 'DK2',
-#   '10,000', 'second-gen', 'original', 'developing..']
-# # puts array.length
-
-
-
-# vvv 6. PROPER NAME FUNCTION vvv
-# vvv [to prep, replace first "array" with input, last "array" with output]
-
-# hold = []
-# proper = []
-# array.each do |word|
-#   if word[0] == word[0].upcase
-#     proper << word
-#   else
-#     if !proper[1].nil?
-#       n = proper.join(' ')
-#       hold << n
-#     end
-#     proper.clear
-#   end
-# end
-
-# print hold
-# print array
-
-# array << hold
-# print array.flatten
-
-
-# working on: 
-  # taking ( "" , [] {} etc) away from everything
-  # taking ( . ) away from ends of words 
+# puts words into three word arrays, as phrases (for phrase searching)
+def make_compound(array)
+  array.each do |word|
+    n = array.index(word)
+    three_word_array = array[n..n+2]
+  end
+end
